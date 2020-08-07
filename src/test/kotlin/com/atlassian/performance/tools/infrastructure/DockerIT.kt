febafaa0e2baffc8b90460f79efb3a699420a1ac
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2020-08-07 10:14:42 +0200

    JPERF-449: Test Docker on multiple Ubuntu versions
    Replace `ssh-ubuntu`-`testcontainers`-`docker-java` triplet with just `docker-java`.
    Runnin Docker-client code allows running mulitple image versions in the same code base.
    The `ssh-ubuntu` distribution via Dockerfiles doesn't permit such parametrization.
    The same approach is used in `virtual-users` tests, so this Docker code could be extracted to `ssh-ubuntu`.

--- src/test/kotlin/com/atlassian/performance/tools/infrastructure/DockerIT.kt
+++ src/test/kotlin/com/atlassian/performance/tools/infrastructure/DockerIT.kt
@@ -1,31 +1,47 @@
 package com.atlassian.performance.tools.infrastructure
 
-import com.atlassian.performance.tools.sshubuntu.api.SshUbuntuContainer
-import com.github.dockerjava.api.model.Bind
-import com.github.dockerjava.api.model.Volume
+import com.atlassian.performance.tools.concurrency.api.submitWithLogContext
+import com.atlassian.performance.tools.infrastructure.api.os.Ubuntu
+import com.atlassian.performance.tools.infrastructure.sshubuntu.SshUbuntuImage.Companion.runSoloSsh
 import org.junit.Test
-import org.testcontainers.containers.GenericContainer
 import java.time.Duration
-import java.util.function.Consumer
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class DockerIT {
+
     @Test
-    fun installWorks() {
-        SshUbuntuContainer(Consumer { enableNestedDocker(it) }).start().use { ssh ->
-            ssh.toSsh().newConnection().use { connection ->
-                //workaround for a bug in Docker download site for bionic
-                val packageFile = "containerd.io_1.2.2-3_amd64.deb"
-                connection.execute("curl -O https://download.docker.com/linux/ubuntu/dists/bionic/pool/edge/amd64/$packageFile", Duration.ofMinutes(3))
-                connection.execute("sudo apt install ./$packageFile", Duration.ofMinutes(3))
+    fun shouldRunOnVariousUbuntuVersions() {
+        val pool = Executors.newCachedThreadPool()
+        listOf("18.04", "16.04")
+            .map { ubuntuVersion ->
+                pool.submitLabelled(ubuntuVersion) {
+                    runSoloSsh(ubuntuVersion) { ssh ->
+                        //workaround for a bug in Docker download site for bionic
+                        Ubuntu().install(ssh, listOf("curl"))
+                        val packageFile = "containerd.io_1.2.2-3_amd64.deb"
+                        ssh.execute("curl -O https://download.docker.com/linux/ubuntu/dists/bionic/pool/edge/amd64/$packageFile", Duration.ofMinutes(3))
+                        ssh.execute("sudo apt install ./$packageFile", Duration.ofMinutes(3))
 
-                Docker().install(connection)
-                DockerImage("hello-world").run(connection)
+                        Docker().install(ssh)
+                        DockerImage("hello-world").run(ssh)
+                    }
+                }
             }
-        }
+            .forEach { it.get() }
     }
+}
 
-    private fun enableNestedDocker(container: GenericContainer<*>) {
-        val dockerDaemonSocket = "/var/run/docker.sock"
-        container.setBinds(listOf(Bind(dockerDaemonSocket, Volume(dockerDaemonSocket))))
+private fun <T> ExecutorService.submitLabelled(
+    label: String,
+    task: () -> T
+): CompletableFuture<T> {
+    return submitWithLogContext(label) {
+        try {
+            task()
+        } catch (e: Exception) {
+            throw Exception(label, e)
+        }
     }
 }
